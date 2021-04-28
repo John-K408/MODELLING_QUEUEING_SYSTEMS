6682aa81b0372d40a4aed5ae8478ca94ece40986
John-K408 <jkoomson23@amherst.edu>
2021-04-28 05:46:48 -0700

    Basic Single Server Queueing System (With test outputs in console) --corrected error

--- GeometricRVGenerator.java
+++ GeometricRVGenerator.java
@@ -8,6 +8,7 @@ public class GeometricRVGenerator {
     }
 
     public int getRandomValue(){
+        numRuns = 0;
         while(true){
             if(numRuns == Integer.MAX_VALUE) return Integer.MAX_VALUE;
             numRuns++;
