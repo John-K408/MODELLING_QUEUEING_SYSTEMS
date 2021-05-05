31ee608bd40ca643fbb9a99fc27e2f1632fe2fd2
John-K408 <jkoomson23@amherst.edu>
2021-05-04 21:02:04 -0400

    Set preliminary values for express job variables to have a system that make general sense

--- QueueSimulation.java
+++ QueueSimulation.java
@@ -20,9 +20,13 @@ public class QueueSimulation {
 
         //int tempJobSize = GeometricGenerator.generateVar(q); //Size for first Job
 
-        //Set expressJobSize to 10;
-        int expressJobSize = 10;
-        int expressJobInterArrival = 9;
+        //Set expressJobSize to 5;
+        int expressJobSize = 5;
+
+        //1/p is the expected value for the interrarrival time of general jobs.
+        //We expect it to take a little longer for express jobs to come in
+        int expressJobInterArrival =(int) (1/p + 2);
+
         //Job firstExpressJob = new Job(0, expressJobSize, expressJobSize); // Arrival time for first job will be zero, Departure Time will be equal to Size of Job
         Server expressServer = new Server(expressJobSize,expressJobInterArrival,null,true);
         //Boundary for express Job -- 30 (this is hypothetical);
@@ -30,13 +34,31 @@ public class QueueSimulation {
         //Let q be the probability for the distribution of job size for general jobs.
         //Let p be the probability for the distribution of interarrival time for general jobs.
         int generalJobSize = GeometricGenerator.generateVar(q);
-        //Job firstGeneralJob = new Job(0,generalJobSize,generalJobSize);
+
+        //Do these because we want to know the average size of general jobs so we can set fixed size of express jobs to something
+        //similar or lower
+
+        //sum of job Sizes for general job
+        int sumJobSizeGeneral  = 0;
+
+        //Number of general jobs worked on so far
+        int numGeneralJobs = 0;
+
+        //Sum of response times for general jobs
+        long sumRTGeneralJobs = 0;
+
+        //Sum of response times for express jobs
+
+        long sumRTExpressJobs = 0;
+
+        //
         Server generalServer = new Server(generalJobSize,GeometricGenerator.generateVar(p),null,false);
 
-        Server server ; // Server with Next Departure time = Departure Time of first Job , Next Arrival time be x~Geometric(p), and job in processing be the firstJob
+        //The server we are currently performing departure or arrival on - could be express or
+        Server server ;
+
 
         //Use booleans to keep track of which activity is happening next for each server
-
         //isArrGen: The next event in the general server is arrival
         boolean isArrGen = true;
 
@@ -49,9 +71,12 @@ public class QueueSimulation {
 
 
 
-        int numJobsArr = 2; // The First Job has already arrived.
+        int numJobsArr = 2; // The First two Jobs have already arrived.
         int numJobsDep = 0; // No Jobs have departed yet.
+        //sumN is the total number of jobs in system at any moment in time
         long sumN = 0;
+
+        //sumT is the total response time for all jobs above k till now.
         long sumT = 0;
 
         while(numJobsArr < n)
@@ -115,11 +140,19 @@ public class QueueSimulation {
                 currentTime = server.nextArrivalTime;
                 System.out.println("Arrival of " + ((server.isExpress)? "express job":"general job") +" at " + currentTime);
                 if(server.isExpress)tempJobSize = expressJobSize;
-                else tempJobSize = GeometricGenerator.generateVar(q); // Generate Size for Job
+
+                else {
+                    // Generate Size for Job
+                    tempJobSize = GeometricGenerator.generateVar(q);
+                    //sumJobSizeGeneral += tempJobSize;
+                    //numGeneralJobs++;
+                }
                 Job tempJob = new Job(currentTime, tempJobSize, server.emptyTime(currentTime)  + tempJobSize); // Generate job
 
                 if(numJobsArr > k)  // The first k boundary exceptions have already arrived
                 {
+                    //get the number of general jobs that arrive in summer over entire iteration
+                    if(!server.isExpress)numGeneralJobs++;
                     sumN += server.numJobs(); //Add current jobs in server
                 }
                 numJobsArr++;
@@ -138,7 +171,8 @@ public class QueueSimulation {
                     server.queue.add(tempJob);
                 }
 
-                server.nextArrivalTime += GeometricGenerator.generateVar(p); // Add a new Inter Arrival Time
+                if(server.isExpress)server.nextArrivalTime += expressJobInterArrival;
+                else server.nextArrivalTime += GeometricGenerator.generateVar(p);
 
                 System.out.println("Next Arrival Time For " + ((server.isExpress)? "express job":"general job") +" is " + server.nextArrivalTime);
 
@@ -153,7 +187,11 @@ public class QueueSimulation {
 
                 if(numJobsArr > k) // The first k boundary exceptions have already arrived
                 {
-                    sumT += server.jobInService.depTime - server.jobInService.arrTime; // Adding Service Time
+                    //Adding response time for each server
+                    long responseTime = server.jobInService.depTime - server.jobInService.arrTime;
+                    if(server.isExpress)sumRTExpressJobs += responseTime;
+                    else sumRTGeneralJobs += responseTime;
+                    //sumT += server.jobInService.depTime - server.jobInService.arrTime; // Adding Service Time
                     numJobsDep++;
                 }
 
@@ -186,6 +224,10 @@ public class QueueSimulation {
             }
         }
 
+       // System.out.println("Average General Job Size: " + sumJobSizeGeneral/numGeneralJobs);
+        System.out.println("Average Response Time for General Jobs: " + (sumRTGeneralJobs+ 0.0)/numGeneralJobs);
+        System.out.println("Average Response Time For Express Jobs: " + (sumRTExpressJobs+ 0.0)/(n - numGeneralJobs - k));
+
 
 //        double expectedN = sumN / (double)(n-k);     // Dividing by total number of jobs that arrived within the boundary
 //        double expectedT = sumT /(double)numJobsDep; // Dividing by total number of jobs that arrived within the boundary
@@ -203,13 +245,15 @@ public class QueueSimulation {
     {
         Scanner scan = new Scanner(System.in);
         double p;
+
+
         do {
 
             System.out.print("Please Enter Value of p in between 0 and 1(or not in range to terminate program): ");
             p = scan.nextDouble();
             if(p <= 1 && p > 0)
             {
-                simulateQueue(100, p,0.5, 2);
+                simulateQueue(200, p,0.1, 50);
             }
 
         } while (p <= 1 && p > 0);
