762fce92cd061bc348cb7e958d09d9f237342b9b
AhsanRandhawa <83049347+AhsanRandhawa@users.noreply.github.com>
2021-05-07 21:40:11 +0500

    Update Job.java

--- Job.java
+++ Job.java
@@ -2,11 +2,13 @@ public class Job {
     public long arrTime;
     public int size;
     public long depTime;
+    public boolean isExpress;
 
-    public Job(long arrTime, int size, long depTime)
+    public Job(long arrTime, int size, long depTime, boolean isExpress)
     {
         this.arrTime = arrTime;
         this.depTime = depTime;
         this.size = size;
+        this.isExpress = isExpress;
     }
 }
